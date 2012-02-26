f11954ab7160d1e039e3f48b8ed335fef005a100
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2012-02-26 03:28:16 +0100

    Removed unused JS and added error pages

--- src/project/settings.py
+++ src/project/settings.py
@@ -179,6 +179,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'cms.context_processors.media',
     'sekizai.context_processors.sekizai',
+    'django.core.context_processors.debug',
 )
 
 CMS_TEMPLATES = (
